f69c459f79aed1096bf80d3ccb51a07f3f55fa6a
Kim Youngsup <ks_ym0522@naver.com>
2023-05-16 17:55:10 +0900

    feat: project build

---
+++ src/main/java/com/example/springbootexercise/SpringbootExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootExerciseApplication.class, args);
+    }
+
+}
